effbcdb16903544ad689d5b91e321565563cb5de
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-09 02:16:41 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  e_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  e_array = e_alphabet.split("")
+  new_chars = []
+
+  arr.sort_by do |sentence|
+    new_chars = sentence.split("")
+    new_chars.collect do |letter|
+      e_array.index(letter)
+    end
+  end
+
 end
